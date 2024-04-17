8931817f4083446a528812c54ad12a81e10605c6
Salman Anjum <salman.anjum94@gmail.com>
2024-04-18 01:51:09 +0530

    episode-3

--- App.js
+++ App.js
@@ -1,46 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-/*
-<div id="parent">
-   <div id="child">
-      <h1>I'm heading<h1>
-   </div>
-</div>
 
-<div id="parent">
-   <div id="child">
-      <h1>I'm heading<h1>
-      <h2>I'm heading2<h2>
-   </div>
-</div>
+//This is how we create React Element using core React
+//React.createElement => ReactElement => JS Object => HTML(render)
+//const heading = React.createElement("h1", {id:"heading"}, "Namaste React");
+//console.log(jsxHeading); //Object
 
-<div id="parent">
-   <div id="child1">
-      <h1>I'm heading1<h1>
-      <h2>I'm heading2<h2>
-   </div>
-   <div id="child2">
-      <h1>I'm heading1<h1>
-      <h2>I'm heading2<h2>
-   </div>
-</div>
+//This is how we create React Element using JSX
+//JSX => Babel transpiles it to React.createElement => ReactElement => JS Object => HTML(render)
+const element = <span>React Element</span>;
+const jsxHeading = (
+  <h1 id="heading" className="head">
+    {element}
+    Namaste React using JSX
+  </h1>
+);
+//console.log(jsxHeading); //Object
 
-*/
+//React Component
+//Everything in React is a component- Header,Footer,Button,Card,Input Box,Title,Search Bar etc.
+//There are two types of components in React - Class Based Components & Functional Components
+//Class Based Components -- OLD WAY OF WRITING CODE
+//Functional Components  -- NEW WAY OF WRITING CODE
 
-// const parent = React.createElement("div", {id:"parent"},
-//                React.createElement("div", {id:"child"}, 
-//                [React.createElement("h1", {}, "I'm heading"),
-//                React.createElement("h2", {}, "I'm heading2")]))
+//React Functional Component
+//React Functional Component is just a normal JS function which returns some piece of JSX code (i.e React Elements)
+//Function that returns a piece of JSX code (i.e React Elements) is Functional Component
 
-const parent = React.createElement("div", {id:"parent"},
-               [React.createElement("div", {id:"child1"},
-               [React.createElement("h1", {}, "I'm heading1"), React.createElement("h2", {}, "I'm heading2")]),
-               ], React.createElement("div", {id:"child2"},
-               [React.createElement("h1", {}, "I'm heading1"), React.createElement("h2", {}, "I'm heading2")]))
+//Syntax 1
+// const HeadingComponent = () => {
+//   return <h1>Namaste React Functional Component</h1>;
+// };
 
+//Syntax 2
+const number = 10000;
+const HeadingComponent = () => (
+  //Inserting Footer component inside HeadingComponent is known as component composition
+  <div id="container">
+    <FooterComponent />
+    <FooterComponent></FooterComponent>
+    {FooterComponent()}
+    <h2>{number}</h2>
+    {jsxHeading}
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
 
-const heading = React.createElement("h1", {id:"heading"}, "Hello World from React");
-//console.log(heading); // Will return Object
+const FooterComponent = () => (
+  <div id="footer">
+    <h1 className="footerSection">This is Footer Section</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-//console.log(root); // Will return Object
-root.render(parent);
+root.render(<HeadingComponent />);
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 #Namaste React
+
+#React.createElement is a JavaScript Object
+
+#Parcel is a beast. It performs the following task
+
+- Dev build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algoritham
+- Catching - Faster Builds
+- Image Optimisation
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - to support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different development and production bundles
+- Transpiling
+
+#JSX is a JavaScript syntax which is easier to create React Elements
+#JSX is not HTML inside JavaScript, it is HTML-like or XML-like syntax
+#Whenever we write some code in JSX, Parcel (Babel) transpiles it before it reaches JS Engine
+#Transpiling means JSX code gets converted into a code that our browsers & react can understand
+#Parcel gives the reponsibility of Transpilation to a JS package, known as Babel
+#Babel converts the JSX code to React.Element, which is a JS Object
+#We have to use cameCase while giving attributues to JSX
+#While writing more than one line of code in JSX, it is mandatory to wrap it in round brackets
+#At the end of the day, JSX is a React Element
+#JSX sanitizes the data coming from an API call, this saving us from becoming a target of an attacker/hacker
